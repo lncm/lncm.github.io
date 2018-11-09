ee0db3c1c5f47a5486dcfb2a4d26e6dcd9f04b3d
Chris Hitchcott <hitchcott@gmail.com>
2018-11-10 00:25:19 +0700

    tweak social media icons

--- src/components/IconLink.jsx
+++ src/components/IconLink.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ExternalLink from './ExternalLink';
 
 export default ({ icon, alt, className, ...props }) => (
-  <ExternalLink {...props} className={`icon-link ${className || ''}`}>
+  <ExternalLink {...props} title={alt} className={`icon-link ${className || ''}`}>
     <img src={icon} alt={alt} />
   </ExternalLink>
 );
--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -10,12 +10,12 @@ import mastodonIcon from '../images/mastodon.svg';
 import gitlabIcon from '../images/gitlab.svg';
 
 export default () => (
-  <div className="social-media">
+  <div className="online-presence">
     <IconLink icon={twitterIcon} alt="Twitter" href="https://twitter.com/lncnx" />
-    <IconLink icon={githubIcon} alt="Twitter" href="https://github.com/lncm" />
-    <IconLink icon={dockerIcon} alt="Twitter" href="https://hub.docker.com/u/lncm/" />
-    <IconLink icon={npmIcon} alt="Twitter" href="https://www.npmjs.com/org/lncm" />
-    <IconLink icon={mastodonIcon} alt="Twitter" href="https://bitcoinhackers.org/@lncm" />
-    <IconLink icon={gitlabIcon} alt="Twitter" href="https://gitlab.com/lncm/" />
+    <IconLink icon={githubIcon} alt="GitHub" href="https://github.com/lncm" />
+    <IconLink icon={dockerIcon} alt="Docker" href="https://hub.docker.com/u/lncm/" />
+    <IconLink icon={npmIcon} alt="NPM" href="https://www.npmjs.com/org/lncm" />
+    <IconLink icon={mastodonIcon} alt="Mastodon" href="https://bitcoinhackers.org/@lncm" />
+    <IconLink icon={gitlabIcon} alt="GitLab" href="https://gitlab.com/lncm/" />
   </div>
 );
